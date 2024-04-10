90c3c84c0fa1917a12c09dc05a32dce4e29f2905
Ben Baron <ben@einsteinx2.com>
2024-04-10 08:35:56 -0500

    feat: Add privacy manifest (#28)

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
                .upToNextMajor(from: "8.0.0")),
       .package(name: "RadarSDK",
                url: "https://github.com/radarlabs/radar-sdk-ios",
-               .upToNextMinor(from: "3.4.0")),
+               .upToNextMinor(from: "3.9.6")),
     ],
     targets: [
         .target(
